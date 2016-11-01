3303fd503799f1d492bc0fc7fabdf15eca20a919
kaje11 <zom.kaje11@gmail.com>
2016-11-01 14:25:26 +0100

    Pierwsza wersja

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject player;
+	Camera myCamera;
+	public float cameraOffset = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		myCamera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (player.transform.position.x);
+		Vector3 cameraPosition = new Vector3 (player.transform.position.x, myCamera.transform.position.y, myCamera.transform.position.z);
+		myCamera.transform.position = cameraPosition;
+	}
+}
