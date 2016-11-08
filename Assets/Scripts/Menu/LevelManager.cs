558c50338a474a143cb7d71c302b6fe5d6eacabb
Nechoya <nechoya@gmail.com>
2016-11-08 08:24:03 +0100

    Dodane main menu

---
+++ Assets/Scripts/Menu/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void SetScene (string name) {
+
+        Application.LoadLevel(name);
+
+    }
+
+}
