2d2288d5f9f7579688b98615fe4f444ae4db55ff
Daxol <daniel.pabjan@edu.uekat.pl>
2016-11-08 17:31:40 +0100

    update

---
+++ Assets/Scripts/LaserScripts/BoltController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltController : MonoBehaviour
+{
+    public float speed=13; // przypisanie w menu minusowej predkosci
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.right * speed;// poruszanie w osi Z czyli do przodu gdy jest .forward ale po korekcie
+                                              // obrotu musimy użyć przemieszczanie na ekranie w prawo
+
+
+    }
+    
+     void OnTriggerEnter(Collider other)
+      {
+          if (other.gameObject.tag == "Enemy")
+          {
+              Destroy(other.gameObject);
+          }
+      }
+     
+
+
+}
