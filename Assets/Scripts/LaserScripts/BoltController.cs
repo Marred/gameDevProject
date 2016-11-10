75c84f0ae557da0cd6c8d3ab751b5c6be37c3e5e
Daxol <daniel.pabjan@edu.uekat.pl>
2016-11-10 16:25:20 +0100

    poprawka laseru - niszczy sie po napotkaniu kolizji

--- Assets/Scripts/LaserScripts/BoltController.cs
+++ Assets/Scripts/LaserScripts/BoltController.cs
@@ -5,12 +5,13 @@ public class BoltController : MonoBehaviour
 {
     public float speed=13; // przypisanie w menu minusowej predkosci
     Rigidbody rb;
+    [SerializeField]private  GameObject gm; // należy przypisać laserBolt 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.right * speed;// poruszanie w osi Z czyli do przodu gdy jest .forward ale po korekcie
                                               // obrotu musimy użyć przemieszczanie na ekranie w prawo
-
+       
 
     }
     
@@ -19,9 +20,15 @@ public class BoltController : MonoBehaviour
           if (other.gameObject.tag == "Enemy")
           {
               Destroy(other.gameObject);
+             Destroy(gm);
           }
       }
-     
-
+    void OnCollisionEnter()
+    {
+        
+            Destroy(this.gm);
+       
+    }
+   
 
 }
