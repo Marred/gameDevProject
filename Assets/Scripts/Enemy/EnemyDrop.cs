3fcc6d2b214db6a8e1c478d8d1d8f6ab6631000c
Marred <nechoya@gmail.com>
2016-11-23 12:28:24 +0100

    Drop(wstępny)

---
+++ Assets/Scripts/Enemy/EnemyDrop.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDrop : MonoBehaviour {
+
+    //[SerializeField]private Transform spawnPlace;
+    //[SerializeField]private int dropChance;
+
+    [SerializeField]GameObject expOrb;
+    [SerializeField]GameObject oxygenOrb;
+    [SerializeField]GameObject healthOrb;
+
+    void OnDestroy()
+    {
+        int randomChance = Random.Range(0, 101);
+
+        if(randomChance<70)
+        {
+            Instantiate(expOrb, transform.position, Quaternion.identity);
+        }
+        else if(randomChance<85)
+        {
+            Instantiate(expOrb, transform.position, Quaternion.identity);
+            Instantiate(oxygenOrb, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(expOrb, transform.position, Quaternion.identity);
+            Instantiate(healthOrb, transform.position, Quaternion.identity);
+        }
+    }
+
+}
