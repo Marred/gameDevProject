29de1ad27c8511b166588b1260ca7efba1d6e315
kaje11 <zom.kaje11@gmail.com>
2016-11-24 13:56:35 +0100

    Naprawa bledu z onDestroy

--- Assets/Scripts/Enemy/EnemyDrop.cs
+++ Assets/Scripts/Enemy/EnemyDrop.cs
@@ -3,30 +3,39 @@ using System.Collections;
 
 public class EnemyDrop : MonoBehaviour {
 
-    private Vector3 shift = new Vector3(0.5f, 0, 0);
+	private Vector3 shift = new Vector3(0.5f, 0, 0);
 
-    [SerializeField]GameObject expOrb;
-    [SerializeField]GameObject oxygenOrb;
-    [SerializeField]GameObject healthOrb;
+	[SerializeField]GameObject expOrb;
+	[SerializeField]GameObject oxygenOrb;
+	[SerializeField]GameObject healthOrb;
 
-    void OnDestroy()
-    {
-        int randomChance = Random.Range(0, 101);
+	private bool isQuitting = false;
 
-        if(randomChance<70)
-        {
-            Instantiate(expOrb, transform.position, Quaternion.identity);
-        }
-        else if(randomChance<85)
-        {
-            Instantiate(expOrb, transform.position+shift, Quaternion.identity);
-            Instantiate(oxygenOrb, transform.position-shift, Quaternion.identity);
-        }
-        else
-        {
-            Instantiate(expOrb, transform.position+shift, Quaternion.identity);
-            Instantiate(healthOrb, transform.position-shift, Quaternion.identity);
-        }
-    }
+	void OnApplicationQuit(){
+		isQuitting = true;
+
+	}
+	void OnDestroy()
+	{
+		if (isQuitting)
+			return;
+
+		int randomChance = Random.Range(0, 101);
+
+		if(randomChance<70)
+		{
+			Instantiate(expOrb, transform.position, Quaternion.identity);
+		}
+		else if(randomChance<85)
+		{
+			Instantiate(expOrb, transform.position+shift, Quaternion.identity);
+			Instantiate(oxygenOrb, transform.position-shift, Quaternion.identity);
+		}
+		else
+		{
+			Instantiate(expOrb, transform.position+shift, Quaternion.identity);
+			Instantiate(healthOrb, transform.position-shift, Quaternion.identity);
+		}
+	}
 
 }
